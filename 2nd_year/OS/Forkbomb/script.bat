c70f7e71542d00f89520d922f6c885ad637334f3
Sudyko <63749891+Sudyko@users.noreply.github.com>
2021-02-17 08:43:40 +0300

    Update script.bat

--- 2nd_year/OS/Forkbomb/script.bat
+++ 2nd_year/OS/Forkbomb/script.bat
@@ -1,4 +1,4 @@
-:: Simple bat-script for grabbing info about amount of processes [ Windows ]
+:: Simple bat-script for grab amount of processes [ Windows ]
 @echo off
 :begin
 tasklist | find /v /n /c "" >> tripple.txt

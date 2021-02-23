5bb7a62186dec5b62ee2176c46d392813538bb43
Sudyko <63749891+Sudyko@users.noreply.github.com>
2021-02-23 15:30:18 +0300

    Update Horner's_method.c

--- 2nd_year/comp_math/[2.1]/Horner's_method.c
+++ 2nd_year/comp_math/[2.1]/Horner's_method.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void main(int argc, char** argv) { // Gorner.c
+void main(int argc, char** argv) {
     if (argc >= 4) {
         double prev = atof(argv[1]), eps = atof(argv[argc - 1]);
         printf("%lg ", prev);
-        for (int i = 2; i < argc - 1; i++) printf("%lg ", prev = prev * eps + atoi(argv[i]));
+        for (int i = 2; i < argc - 1; i++) printf("%lg ", prev = prev * eps + atof(argv[i]));
     }
     else puts("Minimum 3 arguments: polinominal 'x+a' coefficients and eps");
 }

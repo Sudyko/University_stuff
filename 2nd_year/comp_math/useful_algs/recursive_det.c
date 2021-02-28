60ad34178ef2ae0ab3d30ab896fee2e36fac14c0
Sudyko <63749891+Sudyko@users.noreply.github.com>
2021-02-28 23:48:36 +0300

    Update recursive_det.c

--- 2nd_year/comp_math/useful_algs/recursive_det.c
+++ 2nd_year/comp_math/useful_algs/recursive_det.c
@@ -1,29 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct { double** val; int size; } matrix;
+double Get_det(double** matrix, int size) {
+	if (size == 1) return matrix[0][0];
+	else if (size == 2) return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+	else {
+		int sub_j;
+		int minor_sign = 1;
+		int res = 0;
+		double** sub_matrix = (double**)malloc(sizeof(double*) * size - 1);
+		for (size_t i = 0; i < size; i++) sub_matrix[i] = (double*)malloc(sizeof(double) * size - 1);
+		for (int i = 0; i < size; i++) {
+			sub_j = 0;
+			for (int j = 0; j < size; j++)
+				if (i != j)
+					sub_matrix[sub_j++] = matrix[j] + 1;
 
-matrix init_matrix(matrix to_init, int size) {
-	to_init.size = size;
-	to_init.val = (double**)malloc(sizeof(double*) * size);
-	for (int i = 0; i < size; i++) to_init.val[i] = (double*)malloc(sizeof(double) * size);
-	return to_init;
+			res = res + minor_sign * matrix[i][0] * Get_det(sub_matrix, size - 1);
+			minor_sign = -minor_sign;
+		};
+		for (size_t i = 0; i < size - 1; i++) free(sub_matrix[i]);
+		free(sub_matrix);
+		return res;
+	}
 }
 
 int main() {
 	// console input
 	int size;
-	matrix input;
-	scanf("%d", size);
+	char flag = 1;
+	double res = 0;
+	scanf("%d", &size);
 	if (size < 1) {
 		puts("Error: matrix size < 1");
 		return 2;
 	}
-	input = init_matrix(input, size);
+	else if (size == 1) {
+		scanf("%lg", &res);
+		printf("det = %lg", res);
+		return 0;
+	}
+	double** input = (double**)malloc(sizeof(double*) * size); // allocation and filling matrix
 	for (size_t i = 0; i < size; i++) {
+		input[i] = (double*)malloc(sizeof(double) * size);
 		for (size_t j = 0; j < size; j++) {
-			scanf("%lf", &(input.val[i][j]));
+			scanf("%lg", &(input[i][j]));
 		}
 	}
+	if (size == 2) {
+		res += input[0][0] * input[1][1];
+		res -= input[1][0] * input[0][1];
+		printf("det = %lg", res);
+		flag = 0;
+	}
+	if (flag) printf("det = %lg", Get_det(input, size));
+	for (size_t i = 0; i < size; i++) free(input[i]); // free memory
+	free(input);
 	return 0;
 }

61eda814586edfc8e4a37ea4443658e279cbe47d
Sudyko <63749891+Sudyko@users.noreply.github.com>
2021-03-03 00:28:12 +0300

    Update linux_mmap.c

--- 2nd_year/OS/Memory_bomb/linux_mmap.c
+++ 2nd_year/OS/Memory_bomb/linux_mmap.c
@@ -1,17 +1,11 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include <unistd.h>
 #include <sys/mman.h>
 
 int main() {
 	long memp = getpagesize();
 	while(1) {
-		void *mem = mmap(NULL, memp, 0, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
-		if(mem == MAP_FAILED) { // Catch errors
-			perror("mmap");
-		}
+		void *mem = mmap(NULL, memp, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE, 0, 0);
 	}
 	return 0;
 }
-// vim /etc/sysctl.conf
-// [change] vm.max_map_count = 1750000 -- максимальное количество областей карты памяти процесса

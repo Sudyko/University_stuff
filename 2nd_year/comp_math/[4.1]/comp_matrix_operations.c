a9c08b140840444fedbe9757d44ba4212935545d
Sudyko <63749891+Sudyko@users.noreply.github.com>
2021-03-27 10:02:27 +0300

    Update comp_matrix_operations.c

--- 2nd_year/comp_math/[4.1]/comp_matrix_operations.c
+++ 2nd_year/comp_math/[4.1]/comp_matrix_operations.c
@@ -26,16 +26,15 @@ void init_matrix(matrix& mat) {
 	}
 }
 
-matrix copy_matrix(matrix& mat) {
-	matrix tmp = { 0, mat.row, mat.col };
-	tmp.val = (complex<double>**)malloc(sizeof(complex<double>*) * tmp.row);
-	for (size_t i = 0; i < tmp.row; ++i) {
-		tmp.val[i] = (complex<double>*)malloc(sizeof(complex<double>) * tmp.col);
-		for (size_t j = 0; j < tmp.col; ++j) {
-			tmp.val[i][j] = mat.val[i][j];
+void copy_matrix(matrix& mat, matrix& result) {
+	result = { 0, mat.row, mat.col };
+	result.val = (complex<double>**)malloc(sizeof(complex<double>*) * result.row);
+	for (size_t i = 0; i < result.row; ++i) {
+		result.val[i] = (complex<double>*)malloc(sizeof(complex<double>) * result.col);
+		for (size_t j = 0; j < result.col; ++j) {
+			result.val[i][j] = mat.val[i][j];
 		}
 	}
-	return tmp;
 }
 
 void free_matrix(matrix& matrix) {
@@ -43,39 +42,34 @@ void free_matrix(matrix& matrix) {
 	free(matrix.val);
 }
 
-matrix arithm_matrix(matrix& mat_1, matrix& mat_2, char op) {
-	matrix tmp = copy_matrix(mat_1);
-	for (size_t i = 0; i < tmp.row; ++i) {
-		for (size_t j = 0; j < tmp.col; ++j) {
-			if (op != '2') tmp.val[i][j] += mat_2.val[i][j];
-			else tmp.val[i][j] -= mat_2.val[i][j];
+void arithm_matrix(matrix& mat_1, matrix& mat_2, char op, matrix& result) {
+	for (size_t i = 0; i < result.row; ++i) {
+		for (size_t j = 0; j < result.col; ++j) {
+			if (op != '2') result.val[i][j] += mat_2.val[i][j];
+			else result.val[i][j] -= mat_2.val[i][j];
 		}
 	}
-	return tmp;
 }
 
-matrix mul_matrix_by_num(matrix& mat, complex<double>& num) {
-	matrix tmp = copy_matrix(mat);
+void mul_matrix_by_num(matrix& mat, complex<double>& num, matrix& result) {
 	for (size_t i = 0; i < mat.row; ++i) {
 		for (size_t j = 0; j < mat.col; ++j) {
-			tmp.val[i][j] *= num;
+			result.val[i][j] *= num;
 		}
 	}
-	return tmp;
 }
 
-matrix mul_mat_by_mat(matrix& mat_1, matrix& mat_2) {
-	matrix tmp = { 0, mat_1.row, mat_2.col };
-	tmp.val = (complex<double>**)malloc(sizeof(complex<double>*) * tmp.row);
-	for (size_t i = 0; i < tmp.row; ++i) {
-		tmp.val[i] = (complex<double>*)malloc(sizeof(complex<double>) * tmp.col);
-		for (size_t j = 0; j < tmp.col; ++j) {
-			tmp.val[i][j] = 0;
+void mul_mat_by_mat(matrix& mat_1, matrix& mat_2, matrix& result) {
+	result = { 0, mat_1.row, mat_2.col };
+	result.val = (complex<double>**)malloc(sizeof(complex<double>*) * result.row);
+	for (size_t i = 0; i < result.row; ++i) {
+		result.val[i] = (complex<double>*)malloc(sizeof(complex<double>) * result.col);
+		for (size_t j = 0; j < result.col; ++j) {
+			result.val[i][j] = 0;
 			for (size_t k = 0; k < mat_2.row; ++k)
-				tmp.val[i][j] += mat_1.val[i][k] * mat_2.val[k][j];
+				result.val[i][j] += mat_1.val[i][k] * mat_2.val[k][j];
 		}
 	}
-	return tmp;
 }
 
 void print_matrix(matrix mat) {
@@ -88,28 +82,22 @@ void print_matrix(matrix mat) {
 }
 
 int main(int argc, char** argv) {
-	matrix matrix_1, matrix_2;
+	matrix matrix_1 = { 0, 0, 0 }, matrix_2 = { 0, 0, 0 };
 	if (argc == 2) { //file input
 		ifstream fin(argv[1]);
 		if (!fin.is_open()) {
 			puts("Error: incorrect file name.");
-			free_matrix(matrix_1);
-			free_matrix(matrix_2);
 			return 2;
 		}
 		fin >> matrix_1.row >> matrix_1.col;
 		if (matrix_1.row < 1 || matrix_1.col < 1) {
 			puts("Error: matrix_1 row_num or col_num < 1");
-			free_matrix(matrix_1);
-			free_matrix(matrix_2);
 			return 2;
 		}
 		init_matrix(matrix_1, fin);
 		fin >> matrix_2.row >> matrix_2.col;
 		if (matrix_2.row < 1 || matrix_2.col < 1) {
 			puts("Error: matrix_2 row_num or col_num < 1");
-			free_matrix(matrix_1);
-			free_matrix(matrix_2);
 			return 2;
 		}
 		init_matrix(matrix_2, fin);
@@ -119,27 +107,21 @@ int main(int argc, char** argv) {
 		scanf("%d %d", &matrix_1.row, &matrix_1.col);
 		if (matrix_1.row < 1 || matrix_1.col < 1) {
 			puts("Error: matrix_1 row_num or col_num < 1");
-			free_matrix(matrix_1);
-			free_matrix(matrix_2);
 			return 2;
 		}
 		init_matrix(matrix_1);
 		scanf("%d %d", &matrix_2.row, &matrix_2.col);
 		if (matrix_2.row < 1 || matrix_2.col < 1) {
 			puts("Error: matrix_2 row_num or col_num < 1");
-			free_matrix(matrix_1);
-			free_matrix(matrix_2);
 			return 2;
 		}
 		init_matrix(matrix_2);
 	}
 	else {
 		puts("Wrong num of arguments.");
-		free_matrix(matrix_1);
-		free_matrix(matrix_2);
 		return 2;
 	}
-	while(true) {
+	while (true) {
 		char check;
 		matrix result = { 0, 0, 0 };
 		cout << "[1] Sum matrix." << endl
@@ -150,7 +132,8 @@ int main(int argc, char** argv) {
 		cin >> check;
 		if (check == '1' || check == '2') {
 			if (matrix_1.row == matrix_2.row && matrix_1.col == matrix_2.col) {
-				result = arithm_matrix(matrix_1, matrix_2, check);
+				copy_matrix(matrix_1, result);
+				arithm_matrix(matrix_1, matrix_2, check, result);
 				print_matrix(result);
 			}
 			else puts("Error: matrix_1 and matrix_2 have different sizes.");
@@ -158,19 +141,20 @@ int main(int argc, char** argv) {
 		else if (check == '3') {
 			complex<double> num;
 			cout << "Type comp. num." << endl;
-			cin >> num;
-			result = mul_matrix_by_num(matrix_1, num);
+			cin >> num; // Does it need check?
+			copy_matrix(matrix_1, result);
+			mul_matrix_by_num(matrix_1, num, result);
 			print_matrix(result);
 		}
 		else if (check == '4') {
 			if (matrix_1.row == matrix_2.col && matrix_1.col == matrix_2.row) {
-				result = mul_mat_by_mat(matrix_1, matrix_2);
+				mul_mat_by_mat(matrix_1, matrix_2, result);
 				print_matrix(result);
 			}
 			else puts("Error: matrix_1 or matrix_2 has wrong size.");
 		}
 		else if (check == '5') break;
-		free_matrix(result);
+		if(result.val) free_matrix(result);
 	}
 	free_matrix(matrix_1);
 	free_matrix(matrix_2);

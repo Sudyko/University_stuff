697482967c2c32fe9ab0c6181cddd706d3fda102
Sudyko <63749891+Sudyko@users.noreply.github.com>
2021-04-04 20:19:50 +0300

    Update scalar_and_vec_mul.c

--- 2nd_year/comp_math/[6.1]/scalar_and_vec_mul.c
+++ 2nd_year/comp_math/[6.1]/scalar_and_vec_mul.c
@@ -30,7 +30,7 @@ void init_matrix(matrix& mat) {
 }
 
 void free_matrix(matrix& mat) {
-    for (size_t i = 0; i < mat.col; ++i) free(mat.val[i]);
+    for (size_t i = 0; i < mat.row; ++i) free(mat.val[i]);
     free(mat.val);
 }
 
@@ -64,41 +64,41 @@ void print_name(int n) {
     free(res);
 }
 
-void get_new_matrix(matrix& mat, char* pos, matrix* new_mat, int num) {
-    char check = 1;
-    int k = 0;
-    if (!strcmp(pos, "row")) {
-        (*new_mat).row = mat.row - 1;
-        (*new_mat).col = mat.col;
-        (*new_mat).val = (double**)malloc(sizeof(double*) * new_mat->row);
-        for (size_t i = 0; i < new_mat->row; ++i, ++k) {
-            if (i == num - 1 && check) {
-                check = 0;
-                --k;
-                continue;
-            }
-            (*new_mat).val[k] = (double*)malloc(sizeof(double) * new_mat->col);
-            for (size_t j = 0; j < new_mat->col; ++j) {
-                (*new_mat).val[k][j] = mat.val[i][j];
-            }
-        }
-    }
-    else {
-        (*new_mat).row = mat.row;
-        (*new_mat).col = mat.col - 1;
-        (*new_mat).val = (double**)malloc(sizeof(double*) * new_mat->row);
-        for (size_t i = 0; i < new_mat->row; ++i) {
-            (*new_mat).val[i] = (double*)malloc(sizeof(double) * new_mat->col);
-            for (size_t j = 0; j < new_mat->col; ++j, ++k) {
-                if (j == num - 1 && check) {
-                    check = 0;
-                    --k;
-                    break;
-                }
-                (*new_mat).val[i][k] = mat.val[i][j];
-            }
-        }
-    }
+void get_new_matrix(matrix& mat, char* pos, int num, matrix* new_mat) {
+	int k = 0;
+	if (!strcmp(pos, "row")) {
+		(*new_mat).row = mat.row - 1;
+		(*new_mat).col = mat.col;
+		char check = 1;
+		(*new_mat).val = (double**)malloc(sizeof(double*) * (*new_mat).row);
+		for (size_t i = 0; i < (*new_mat).row; ++i, ++k) {
+			if ((i == num - 1) && check) {
+				check = 0;
+				++k;
+			}
+			(*new_mat).val[i] = (double*)malloc(sizeof(double) * (*new_mat).col);
+			for (size_t j = 0; j < (*new_mat).col; ++j) {
+				(*new_mat).val[i][j] = mat.val[k][j];
+			}
+		}
+	}
+	else {
+		(*new_mat).row = mat.row;
+		(*new_mat).col = mat.col - 1;
+		(*new_mat).val = (double**)malloc(sizeof(double*) * (*new_mat).row);
+		for (size_t i = 0; i < (*new_mat).row; ++i) {
+			char check = 1;
+			k = 0;
+			(*new_mat).val[i] = (double*)malloc(sizeof(double) * (*new_mat).col);
+			for (size_t j = 0; j < (*new_mat).col; ++j, ++k) {
+				if ((j == num - 1) && check) {
+					check = 0;
+					++k;
+				}
+				(*new_mat).val[i][j] = mat.val[i][k];
+			}
+		}
+	}
 }
 
 int main(int argc, char** argv) {
@@ -162,9 +162,9 @@ int main(int argc, char** argv) {
                 else printf("%.2lfa - %.2lfb\n", vec[0], vec[1]);
                 continue;
             }
-            if (mat.row > 2) {
+            else if (mat.row > 2) {
                 matrix new_mat = { 0, 0 };
-                get_new_matrix(mat, pos1, &new_mat, num1);
+                get_new_matrix(mat, pos1, num1, &new_mat);
                 print_matrix(new_mat);
                 free_matrix(new_mat);
             }
